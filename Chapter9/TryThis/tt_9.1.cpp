689bea07fc0dd635a748d2cbdc3fe4e0442f0977
AnhDungPham2901 <dungpham290198@gmail.com>
2026-02-19 14:33:37 +0700

    add try this

---
+++ Chapter9/TryThis/tt_9.1.cpp
@@ -0,0 +1,10 @@
+#include "../../PPPHeaders.h"
+
+int main() {
+    int birthday_year = 1998;
+    cout << "Decimal: " << '\t' <<dec << birthday_year << '\n';
+    cout << "Hexadecimal: " << '\t' <<hex << birthday_year << '\n';
+    cout << "Octal: " << '\t' <<oct << birthday_year << '\n';
+    cout << "My age: " << '\t' <<dec << 28 << '\n';
+    return 0;
+}
